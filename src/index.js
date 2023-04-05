55995605ad0d1df0622f986f3d4a73e261870491
monte-dev <119732972+monte-dev@users.noreply.github.com>
2023-04-05 22:37:16 +0100

    update: refactor states to use redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { createRoot } from 'react-dom/client';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './App';
-import 'font-awesome/css/font-awesome.min.css'
+import 'font-awesome/css/font-awesome.min.css';
 
 import './styles/normalize.scss';
 import './styles/global.scss';
@@ -9,5 +12,9 @@ const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 
 root.render(
-  <App />,
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>
 );
